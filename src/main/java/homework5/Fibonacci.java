05ce9b8f18698dc1b6e24eda92cb35beced7f05d
Giorgi Gogua <giogogu@icloud.com>
2026-06-02 03:34:44 -0700

    [Giorgi G.] HW5 - Fibonacci

--- src/main/java/homework5/Fibonacci.java
+++ src/main/java/homework5/Fibonacci.java
@@ -1,4 +1,25 @@
+/** @author Giorgi Gogua */
 package homework5;
 
 public class Fibonacci {
+    public static long fib(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        long previous = 0;
+        long current = 1;
+
+        for (int i = 2; i <= n; i++) {
+            long next = previous + current;
+            previous = current;
+            current = next;
+        }
+
+        return current;
+    }
+
+    public static void main(String[] args) {
+        int age = 33;
+        System.out.println(fib(age));
+    }
 }
