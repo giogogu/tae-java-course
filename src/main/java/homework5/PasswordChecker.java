8d0647ac74724cc0279d1fd6b34e95b3cf273d44
Giorgi Gogua <giogogu@icloud.com>
2026-06-02 03:34:34 -0700

    [Giorgi G.] HW5 - PasswordChecker

--- src/main/java/homework5/PasswordChecker.java
+++ src/main/java/homework5/PasswordChecker.java
@@ -1,4 +1,27 @@
+/** @author Giorgi Gogua */
 package homework5;
 
 public class PasswordChecker {
+    public static boolean isStrong(String password) {
+        boolean hasDigit = false;
+        boolean hasUpperCase = false;
+
+        for (int i = 0; i < password.length(); i++) {
+            char ch = password.charAt(i);
+
+            if (Character.isDigit(ch)) {
+                hasDigit = true;
+            }
+
+            if (Character.isUpperCase(ch)) {
+                hasUpperCase = true;
+            }
+        }
+
+        return password.length() >= 8 && hasDigit && hasUpperCase;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isStrong("Hello123"));
+    }
 }
