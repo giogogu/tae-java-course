40dc31a8d8be08db2120d5a270f4a1a90cd3d7b4
Giorgi Gogua <giogogu@icloud.com>
2026-06-02 03:33:56 -0700

    [Giorgi G.] HW5 - SumEvens

--- src/main/java/homework5/SumEvens.java
+++ src/main/java/homework5/SumEvens.java
@@ -1,4 +1,21 @@
+/** @author Giorgi Gogua */
 package homework5;
 
 public class SumEvens {
+    public static int sumEvens(int from, int to) {
+        int sum = 0;
+
+        for (int i = from; i <= to; i++) {
+            if (i % 2 == 0) {
+                sum += i;
+            }
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int age = 33;
+        System.out.println(sumEvens(1, age * 2));
+    }
 }
