5511a5deaec2cae701c78cf22dad0ca82148fc46
Giorgi Gogua <giogogu@icloud.com>
2026-06-05 00:50:56 -0700

    [Giorgi G.] HW6 - Calculator

---
+++ src/main/java/homework6/Calculator.java
@@ -0,0 +1,36 @@
+/** @author Giorgi Gogua */
+package homework6;
+
+public class Calculator {
+    public double calculate(double a, double b, char op) {
+
+        switch (op) {
+            case '+':
+                return a + b;
+
+            case '-':
+                return a - b;
+
+            case '*':
+                return a * b;
+
+            case '/':
+                if (b == 0) {
+                    return Double.NaN;
+                }
+                return a / b;
+
+            default:
+                return Double.NaN;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Calculator calc = new Calculator();
+
+        System.out.println(calc.calculate(10, 3, '+'));
+        System.out.println(calc.calculate(10, 3, '*'));
+        System.out.println(calc.calculate(10, 0, '/'));
+    }
+}
