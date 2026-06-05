2269a3019771852873e5bf1158e2e7d4b0e7dd32
Giorgi Gogua <giogogu@icloud.com>
2026-06-05 00:51:10 -0700

    [Giorgi G.] HW6 - Student

--- src/main/java/homework6/Student.java
+++ src/main/java/homework6/Student.java
@@ -1,4 +1,41 @@
+/** @author Giorgi Gogua */
 package homework6;
 
 public class Student {
+    private String name;
+    private int[] grades;
+
+    public Student(String name, int[] grades) {
+        this.name = name;
+        this.grades = grades;
+    }
+
+    public double averageGrade() {
+
+        int sum = 0;
+
+        for (int grade : grades) {
+            sum += grade;
+        }
+
+        return (double) sum / grades.length;
+    }
+
+    public String getStatus() {
+
+        if (averageGrade() >= 60) {
+            return "Pass";
+        } else {
+            return "Fail";
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Student student =
+                new Student("Ana", new int[]{85, 92, 78});
+
+        System.out.println(student.averageGrade());
+        System.out.println(student.getStatus());
+    }
 }
