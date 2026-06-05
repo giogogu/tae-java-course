86b500535937c620fef40cc8e7acdf5cad39ae85
Giorgi Gogua <giogogu@icloud.com>
2026-06-05 00:34:09 -0700

    [Giorgi G.] HW6 - AnimalDog

--- src/main/java/homework6/Dog.java
+++ src/main/java/homework6/Dog.java
@@ -1,4 +1,18 @@
+/** @author Giorgi Gogua */
 package homework6;
 
-public class Dog {
+public class Dog extends Animal{
+    public Dog(String name) {
+        super(name);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(name + ": Bark!");
+    }
+
+    public static void main(String[] args) {
+        Dog dog = new Dog("Rex");
+        dog.makeSound();
+    }
 }
