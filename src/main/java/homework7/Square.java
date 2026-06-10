7ff4a37e342b005d67ea4f90f39982ed80ffeb3f
Giorgi Gogua <giogogu@icloud.com>
2026-06-09 17:03:08 -0700

    [Giorgi G.] HW7 - Drawable interface

---
+++ src/main/java/homework7/Square.java
@@ -0,0 +1,9 @@
+/** @author Giorgi Gogua */
+package homework7;
+
+public class Square implements Drawable {
+    @Override
+    public void draw() {
+        System.out.println("Drawing Square");
+    }
+}
