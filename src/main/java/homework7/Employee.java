57a2f48322d8911b02b8d097739e76642983a76e
Giorgi Gogua <giogogu@icloud.com>
2026-06-09 17:03:43 -0700

    [Giorgi G.] HW7 - Employee hierarchy

---
+++ src/main/java/homework7/Employee.java
@@ -0,0 +1,16 @@
+/** @author Giorgi Gogua */
+package homework7;
+
+public abstract class Employee {
+    protected String name;
+
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    public abstract double calculateSalary();
+
+    public void printInfo() {
+        System.out.println("Employee: " + name);
+    }
+}
