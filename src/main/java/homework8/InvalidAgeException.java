bafc26575cd134e92c5918837769d3f89af1c5e7
Giorgi Gogua <giogogu@icloud.com>
2026-06-13 01:38:02 -0700

    [Giorgi G.] HW8 - AgeValidator

---
+++ src/main/java/homework8/InvalidAgeException.java
@@ -0,0 +1,9 @@
+/** @author Giorgi Gogua */
+package homework8;
+
+public class InvalidAgeException extends Exception {
+
+    public InvalidAgeException(String message) {
+        super(message);
+    }
+}
